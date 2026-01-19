77461a45a591f24c4eb6c8c208a62fa7c89d7810
Vitor <adrianovor@gmail.com>
2026-01-18 21:11:01 -0300

    chore(firebase): configuração do ambiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:knuckle_bones/core/data/user_model.dart';
+import 'package:knuckle_bones/firebase_options.dart';
 import 'package:knuckle_bones/my_app.dart';
+
 //emulator -avd Pixel4 -accel on -gpu host
 
 void _setupDependencies() {
@@ -16,6 +19,7 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   _setupDependencies();
   runApp(const MyApp());
 }
